53b3abfa4414add5759951c10906797ddd4c634d
Kai Yi Lee <kaiyisg@yahoo.com.sg>
2017-02-12 00:27:12 -0800

    SplashActivity setup

--- app/src/main/java/com/noc/keen/SplashActivity.java
+++ app/src/main/java/com/noc/keen/SplashActivity.java
@@ -4,13 +4,16 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
-    private static final int SPLASH_LENGTH = 400;
+    private static final int SPLASH_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
