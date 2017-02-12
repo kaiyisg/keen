546874d25bfb830905521da79b1c6a60f029d691
Kai Yi Lee <kaiyisg@yahoo.com.sg>
2017-02-12 00:05:00 -0800

    Refactor LandingPage button

--- app/src/main/java/com/noc/keen/LandingPage.java
+++ app/src/main/java/com/noc/keen/LandingPage.java
@@ -1,5 +1,6 @@
 package com.noc.keen;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,27 +11,31 @@ import android.widget.Button;
  * Created by JiaWern on 11/2/17.
  */
 
-public class LandingPage extends AppCompatActivity implements View.OnClickListener{
+public class LandingPage extends AppCompatActivity {
+
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+        mContext = this;
 
-        Intent intent = getIntent();
         final Button buy_button = (Button) findViewById(R.id.buy_button);
-        buy_button.setOnClickListener(this);
+        buy_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, CustomTenderActivity.class);
+                startActivity(intent);
+            }
+        });
 
         final Button info_button = (Button) findViewById(R.id.info_button);
-        info_button.setOnClickListener(this);
-    }
+        info_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public void onClick(View v) {
-        Intent intent = new Intent();
-        if (v.getTag().equals(getResources().getString(R.string.buynow_value))) {
-            intent = new Intent(this, CustomTenderActivity.class);
-        } else if (v.getTag().equals(getResources().getString(R.string.getinfo_value))) {
-            intent = new Intent(this, CustomTenderActivity.class);
-        }
-        startActivity(intent);
+            }
+        });
     }
 }
